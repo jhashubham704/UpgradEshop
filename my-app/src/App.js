518dbdd55c698b2cc2551558310fa0eefc7b5898
jhashubham704 <jhashubham704@gmail.com>
2023-09-23 14:57:46 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './common/Navbar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
